efabccde39b48051195fc8e9d06b5239fa3c0f1b
GCT ERP <gct.erp.project@gmail.com>
2023-02-11 17:40:04 +0530

    Students Module - Changes made in table

--- pages/admin/students.js
+++ pages/admin/students.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 import Dropdown from "../../utilities/Dropdown"
 import Button from "../../utilities/Button"
+import Search from "../../utilities/Search"
 import Table from "../../utilities/Table"
 
 const Students = () => {
@@ -21,11 +22,13 @@ const Students = () => {
 
     for(let i = 0; i < 7; i++)  result.push(result[0])
 
-    const [ branch, setBranch ] = useState("ALL")
-    const [ semester, setSemester ] = useState("ALL")
+    const [ branch, setBranch ] = useState("All")
+    const [ semester, setSemester ] = useState("All")
     const [ type, setType ] = useState("regular")
     const [ change, setChange ] = useState({ key: null, value: {} })
     const [ data, setData ] = useState(result)
+    const [ filter, setFilter ] = useState(Object.keys(data[0])[0])
+    const [ search, setSearch ] = useState("")
     
     useEffect(() => {
 
@@ -34,7 +37,7 @@ const Students = () => {
 
     }, [change])
 
-    const filterCheck = (doc) => (branch == "ALL" ? true : doc.branch == branch) && (semester == "ALL" ? true : doc.sem == semester) && doc.type == type.toLowerCase()
+    const filterCheck = (doc) => (branch == "All" ? true : doc.branch == branch) && (semester == "All" ? true : doc.sem == semester) && doc.type == type.toLowerCase() && doc[filter.toLowerCase()].toString().includes(search)
 
     const getKeyChain = () => {
         let keys = []
@@ -44,9 +47,10 @@ const Students = () => {
 
     return ( <> 
         <div className="flex space-x-6">
-            <Dropdown name="Branch" update={setBranch} data={[ "ALL", "CIVIL", "MECH", "ECE", "EEE", "EIE", "CSE", "IT", "IBT" ]} />
-            <Dropdown name="Semester" update={setSemester} data={[ "ALL", 1, 2, 3, 4, 5, 6, 7, 8 ]}/>
+            <Dropdown name="Branch" update={setBranch} data={[ "All", "CIVIL", "MECH", "ECE", "EEE", "EIE", "CSE", "IT", "IBT" ]} />
+            <Dropdown name="Semester" update={setSemester} data={[ "All", 1, 2, 3, 4, 5, 6, 7, 8 ]}/>
             <Dropdown name="Type" update={setType} data={[ "regular", "lateral", "transfer" ]}/> 
+            <Search options={Object.keys(data[0]).map(key => key.charAt(0).toUpperCase() + key.slice(1))} filter={filter} setFilter={setFilter} search={search} update={setSearch}/>
         </div><br/>
         <Table editable update={setChange} keys={getKeyChain(data)} data={data.filter(doc => filterCheck(doc))}/><br/>
         <div className="flex mt-5 space-x-10">
